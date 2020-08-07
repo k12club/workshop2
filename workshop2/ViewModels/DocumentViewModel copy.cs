b6d90a08dedaf8238cf56f1967365d1c0b24d46d
katoon <toonysalin@gmail.com>
2020-08-07 16:33:44 +0700

    test

---
+++ workshop2/ViewModels/DocumentViewModel copy.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace workshop2.ViewModels {
+        public class DocumentViewModels {
+
+                public string Name { get; set; }
+                public List<IFormFile> file { get; set; }
+
+                        //hhhh
+        }
+}
