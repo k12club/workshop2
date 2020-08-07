1c94eb7abf3495aef19af6c0c7f697def16f6657
charp <59310494@go.buu.ac.th>
2020-08-07 16:27:10 +0700

    Doc2

---
+++ workshop2/Models/Document2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace workshop2.Models {
+    public class Document2 {
+        [Key]
+        [DatabaseGenerated (DatabaseGeneratedOption.Identity)]
+
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long DocumentStatusId { get; set; } = 1;
+        public virtual DocumentStatus DocumentStatus { get; set; }
+        public long EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+        public long SecretId { get; set; } = 1;
+        public virtual Secret Secret { get; set; }
+        public DateTime Create_At { get; set; } = DateTime.Now;
+
+        public ICollection<DocumentFile> DocumentFiles { get; set; }
+    }
+}
